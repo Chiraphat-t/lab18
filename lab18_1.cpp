5ebab9d5534e1a8d2973cc013c9c1a55c4f83c81
Chiraphat-t <chiraphat.ttt@gmail.com>
2025-02-09 15:41:45 +0700

    1

--- lab18_1.cpp
+++ lab18_1.cpp
@@ -5,8 +5,15 @@ struct Rect{
 	double x,y,w,h;
 };
 
-double overlap(){
-
-
-
+double overlap(Rect r1,Rect r2){
+	double r1R = r1.x + r1.w;
+	double r1B = r1.y - r1.h;
+	double r2R = r2.x + r2.w;
+	double r2B = r2.y - r2.h;
+	
+	double X = min(r1R,r2R)- max(r1.x,r2.x);
+	double Y = min(r1.y,r2.y)- max(r1B,r2B);
+	if (X>0 && Y>0)	{
+		return X * Y;}
+	else return 0;
 }
